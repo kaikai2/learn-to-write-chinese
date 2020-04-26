be1ac1979df46806321acdf6840575cb1130b240
Zhang, Zikai (Data61, Clayton) <Zikai.Zhang@data61.csiro.au>
2020-04-26 17:13:34 +1000

    replay voice text

--- src/view/VoiceText.js
+++ src/view/VoiceText.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React from 'react'
 
+import {Button} from 'react-bootstrap'
+import {GiSpeaker} from 'react-icons/gi'
 import { speak } from '../module/speak'
 
 class VoiceText extends React.Component {
 
     componentDidMount(){
         speak(this.props.text)
+    }
 
+    doSpeak() {
+        speak(this.props.text)
     }
 
     render() {
         return (
             <>
                 {this.props.text}
+                {" "}
+                <Button variant="outline-info" onClick={this.doSpeak.bind(this)} >
+                    <GiSpeaker  />
+                </Button>
             </>
         )
     }
