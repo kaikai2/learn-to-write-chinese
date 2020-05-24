58c973029f1d8870f7b3b6c691f6a4891f303928
Zhang, Zikai (Data61, Clayton) <Zikai.Zhang@data61.csiro.au>
2020-05-24 21:51:03 +1000

    adjust bg of char

--- src/view/HanziField.js
+++ src/view/HanziField.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 
 class HanziField extends React.Component {
-
+    borderStrokeWidth() {
+        return Math.max(2, Math.min(40, Math.floor((this.props.size || 100) / 25)))
+    }
     render() {
         return (
             <g>
                 <line x1="0" y1="0" 
-                    x2={this.props.size || 100} y2={this.props.size || 100} stroke="#DDD" />
+                    x2={this.props.size || 100} y2={this.props.size || 100} stroke="lightpink" />
                 <line x1={this.props.size || 100} y1="0"
-                    x2="0" y2={this.props.size || 100} stroke="#DDD" />
+                    x2="0" y2={this.props.size || 100} stroke="lightpink" />
                 <line x1={(this.props.size || 100)/2} y1="0" 
-                    x2={(this.props.size || 100) / 2} y2={(this.props.size || 100)} stroke="#DDD" />
+                    x2={(this.props.size || 100) / 2} y2={(this.props.size || 100)} stroke="lightpink" />
                 <line x1="0" y1={(this.props.size || 100)/2} 
-                    x2={(this.props.size || 100)} y2={(this.props.size || 100)/2} stroke="#DDD" />
+                    x2={(this.props.size || 100)} y2={(this.props.size || 100)/2} stroke="lightpink" />
                 <line x1="0" y1="0" 
-                    x2="0" y2={(this.props.size || 100) } strokeWidth="2" stroke="#000" />
+                    x2="0" y2={(this.props.size || 100) } 
+                    strokeWidth={this.borderStrokeWidth()} stroke="pink" />
                 <line x1={(this.props.size || 100)} y1="0" 
-                    x2={(this.props.size || 100) } y2={(this.props.size || 100) } strokeWidth="2" stroke="#000" />
+                    x2={(this.props.size || 100) } y2={(this.props.size || 100) } 
+                    strokeWidth={this.borderStrokeWidth()} stroke="pink" />
                 <line x1="0" y1="0" 
-                    x2={(this.props.size || 100) } y2="0" strokeWidth="2" stroke="#000" />
+                    x2={(this.props.size || 100) } y2="0" 
+                    strokeWidth={this.borderStrokeWidth()} stroke="pink" />
                 <line x1="0" y1={(this.props.size || 100) } 
-                    x2={(this.props.size || 100)} y2={(this.props.size || 100) } strokeWidth="2" stroke="#000" />
+                    x2={(this.props.size || 100)} y2={(this.props.size || 100) } 
+                    strokeWidth={this.borderStrokeWidth()} stroke="pink" />
             </g>
         )
     }
