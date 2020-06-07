051499de70e4a135dd99cc75813932644550e0a7
Zhang, Zikai (Data61, Clayton) <Zikai.Zhang@data61.csiro.au>
2020-06-07 13:53:08 +1000

    adjust hanzi field background.

--- src/view/HanziField.js
+++ src/view/HanziField.js
@@ -3,30 +3,34 @@ import React from 'react';
 
 class HanziField extends React.Component {
     borderStrokeWidth() {
-        return Math.max(2, Math.min(40, Math.floor((this.props.size || 100) / 25)))
+        return Math.max(2, Math.min(40, Math.floor((this.props.size || 100) / 50)))
     }
     render() {
         return (
             <g>
-                <line x1="0" y1="0" 
-                    x2={this.props.size || 100} y2={this.props.size || 100} stroke="lightpink" />
-                <line x1={this.props.size || 100} y1="0"
-                    x2="0" y2={this.props.size || 100} stroke="lightpink" />
-                <line x1={(this.props.size || 100)/2} y1="0" 
-                    x2={(this.props.size || 100) / 2} y2={(this.props.size || 100)} stroke="lightpink" />
-                <line x1="0" y1={(this.props.size || 100)/2} 
-                    x2={(this.props.size || 100)} y2={(this.props.size || 100)/2} stroke="lightpink" />
-                <line x1="0" y1="0" 
-                    x2="0" y2={(this.props.size || 100) } 
+                <line x1={this.borderStrokeWidth()} y1={this.borderStrokeWidth()}
+                    x2={(this.props.size || 100) - this.borderStrokeWidth()} y2={(this.props.size || 100) - this.borderStrokeWidth()} stroke="lightpink" />
+                <line x1={(this.props.size || 100) - this.borderStrokeWidth()} y1={this.borderStrokeWidth()}
+                    x2={this.borderStrokeWidth()} y2={(this.props.size || 100) - this.borderStrokeWidth()} stroke="lightpink" />
+                <line x1={(this.props.size || 100)/2} y1={this.borderStrokeWidth()} 
+                    x2={(this.props.size || 100) / 2} y2={(this.props.size || 100) - this.borderStrokeWidth()} stroke="lightpink" />
+                <line x1={this.borderStrokeWidth()} y1={(this.props.size || 100)/2} 
+                    x2={(this.props.size || 100) - this.borderStrokeWidth()} y2={(this.props.size || 100)/2} stroke="lightpink" />
+                <line x1={this.borderStrokeWidth()} y1={this.borderStrokeWidth()} 
+                    x2={this.borderStrokeWidth()} y2={(this.props.size || 100) - this.borderStrokeWidth()} 
+                    strokeLinecap="round"
                     strokeWidth={this.borderStrokeWidth()} stroke="pink" />
-                <line x1={(this.props.size || 100)} y1="0" 
-                    x2={(this.props.size || 100) } y2={(this.props.size || 100) } 
+                <line x1={(this.props.size || 100) - this.borderStrokeWidth()} y1={this.borderStrokeWidth()} 
+                    x2={(this.props.size || 100) - this.borderStrokeWidth()} y2={(this.props.size || 100) - this.borderStrokeWidth()} 
+                    strokeLinecap="round"
                     strokeWidth={this.borderStrokeWidth()} stroke="pink" />
-                <line x1="0" y1="0" 
-                    x2={(this.props.size || 100) } y2="0" 
+                <line x1={this.borderStrokeWidth()} y1={this.borderStrokeWidth()} 
+                    x2={(this.props.size || 100) - this.borderStrokeWidth()} y2={this.borderStrokeWidth()} 
+                    strokeLinecap="butt"
                     strokeWidth={this.borderStrokeWidth()} stroke="pink" />
-                <line x1="0" y1={(this.props.size || 100) } 
-                    x2={(this.props.size || 100)} y2={(this.props.size || 100) } 
+                <line x1={this.borderStrokeWidth()} y1={(this.props.size || 100) - this.borderStrokeWidth()} 
+                    x2={(this.props.size || 100) - this.borderStrokeWidth()} y2={(this.props.size || 100) - this.borderStrokeWidth()} 
+                    strokeLinecap="butt"
                     strokeWidth={this.borderStrokeWidth()} stroke="pink" />
             </g>
         )
